4ff9f363a3b68d995d537ac83dbe1d58bce3d9d4
tane-git <tane.alexander.wilson@gmail.com>
2022-05-03 17:23:00 +1200

    add other packages from example for rollupOptions plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// other imports from example:
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+
 const packageJson = require('./package.json')
 
 export default defineConfig({
@@ -22,13 +29,13 @@ export default defineConfig({
           sourcemap: true,
         }
       ],
-      // plugins: [
-      //   peerDepsExternal(),
-      //   resolve(),
-      //   commonjs(),
-      //   typescript({ useTsconfigDeclarationDir: true }),
-      //   postcss()
-      // ]
+      plugins: [
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
+        typescript({ useTsconfigDeclarationDir: true }),
+        postcss()
+      ]
     }
   }
 })
